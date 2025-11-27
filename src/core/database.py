1e11e8fe4503062e9d2484ff1c4abeec00fab84f
PlatovD <platovdima6@gmail.com>
2025-11-27 16:36:56 +0300

    Лечащий

--- src/core/database.py
+++ src/core/database.py
@@ -1,9 +1,18 @@
 # инициализация соединений с базами данных
 from sqlalchemy import create_engine
-from .config import SQLALCHEMY_DATABASE_URL
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+from core.config import SQLALCHEMY_DATABASE_URL
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
     pool_recycle=3600  # закрытие соединений при бездействии
 )
 
+Base = declarative_base()
+session_maker = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False
+)
