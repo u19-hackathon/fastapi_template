7fdf82c0539bb0e8f82bee592013dd3cb601cd34
Sergey Pronin <sergeypronin.dev@gmail.com>
2025-11-28 14:27:18 +0300

    feat(parser): добавлен адаптер UploadFile -> tempfile

--- src/modules/parser/__init__.py
+++ src/modules/parser/__init__.py
@@ -1,20 +1,20 @@
 from .models import ParsedDocument
-from .services import parse_file, ParserRegistry
+from .services import parse_file, parse_upload_file, ParserRegistry
 from .txt_parser import TxtParser
 from .docx_parser import DocxParser
 from .pdf_parser import PdfParser
-from .image_parser import ImageParser  # Добавляем импорт
+from .image_parser import ImageParser
 from .rtf_parser import RtfParser
 
 
 __all__ = [
     "ParsedDocument",
     "parse_file",
+    "parse_upload_file",
     "ParserRegistry",
     "TxtParser",
     "DocxParser",
     "PdfParser",
     "ImageParser",
     "RtfParser",
-
 ]
